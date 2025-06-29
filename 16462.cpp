fcebd1be444392442ae5f047801bba3d7d56e73f
Kang JooYoung <joo.zero.10@gmail.com>
2025-06-30 00:21:21 +0900

    한양대_'나교수' 교수님의 악필

--- 16462.cpp
+++ 16462.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
@@ -13,8 +14,29 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>score;
 
+        if(score >= 100) total += 100;
+        else{
+            int one=0;
+            int two=0;
+
+            switch(score/10){
+                case 6: case 9: one =90; break;
+                case 0: break;
+                default : one = score / 10 * 10;
+
+            }
+            switch(score % 10){
+                 case 0: case 6: case 9: two = 9; break;
+                 default : two = score % 10;
+            }
+
+            total = total + one + two;
+            
+        }
         
     }
+    
+    cout << round((double)total/n) ;
 
     return 0;
 }
