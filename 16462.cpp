6a6e2f1924cbf24fb9ebecfd21002988aba9b19c
Kang JooYoung <joo.zero.10@gmail.com>
2025-06-29 23:57:37 +0900

    한양대 알고리즘대회 문제_하던거

---
+++ 16462.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    int total=0;
+    int score=0;
+
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin>>score;
+
+        
+    }
+
+    return 0;
+}
